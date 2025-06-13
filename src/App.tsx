79331e9e612d95235ead46f6b7420ff93403313b
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-06-13 14:54:42 -0300

    api key

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-
 import './App.css'
+import {useRef} from "react";
+import axios from "axios";
 
 function App() {
 
-  return (
-    <>
-      <h1>Test</h1>
-    </>
-  )
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    async function search() {
+        const city: string = inputRef.current!.value;
+        const apiKey: string = import.meta.env.VITE_OPEN_WEATHER_API_KEY;
+
+        const apiUrl: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+        console.log(city);
+        const data = await axios.get(apiUrl);
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <h1>Weather React</h1>
+            <input ref={inputRef} type="text" placeholder="Enter city name"/>
+            <button onClick={search}>Search</button>
+        </div>
+    )
 }
 
 export default App
